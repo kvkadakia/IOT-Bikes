e314853ee98bc0fc64d0e06646c45163daf4bd6e
Amrish Jhaveri <ajhave5@uic.edu>
2018-10-13 17:58:53 -0500

    Node application deployed on dragonboard 410c

--- NodeAppDragon/routes/geolocationdata.js
+++ NodeAppDragon/routes/geolocationdata.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var axios = require('axios');
 
 
-router.post('/', function (req, res) {
+router.post('/', /*async*/ function (req, responseMain) {
     // var data = req.body;
     console.log(req.body);
     // console.log("longitude:"+req.body.longitude);
@@ -13,13 +13,36 @@ router.post('/', function (req, res) {
     //console.log(req.headers);
     //fs.writeFileSync('./public/json/' + data.deviceId + '.json', JSON.stringify(req.body, undefined, 2));
 
-    axios.get('https://smart-bike-share.appspot.com/updateLocation?authuser='+req.body.userId+'&lat='+req.body.lat+'&longi='+req.body.long+'&cycleID='+req.body.deviceId)
+    // axios.get('https://smart-bike-share.appspot.com/updateLocation?authuser='+req.body.userId+'&lat='+req.body.lat+'&longi='+req.body.long+'&cycleID='+req.body.deviceId)
+    //     .then(function(response){
+    //         console.log(response.data);
+    //         res.send('respond with a resource:'+response.data);
+    //     }).catch(function(error){
+    //     console.log(error);
+    // });
+
+    /*console.log(JSON.stringify(
+        {
+            authuser: req.body.authuser,
+            lat: req.body.lat,
+            longi: req.body.longi,
+            cycleID: req.body.cycleID
+        }));*/
+
+     /*await */axios.post('https://smart-bike-share.appspot.com/updateLocations',
+        {
+            authuser: req.body.authuser,
+            lat: req.body.lat,
+            longi: req.body.longi,
+            cycleID: req.body.cycleID
+        })
+    // console.log("Completed");
         .then(function(response){
             console.log(response.data);
-            res.send('respond with a resource:'+response.data);
+            responseMain.send('respond with a resource:'+response.data);
         }).catch(function(error){
         console.log(error);
     });
-    // res.send("Success");
+    // responseMain.send("Success");
 });
 module.exports = router;
--- NodeAppDragon/routes/sensors.js
+++ NodeAppDragon/routes/sensors.js
@@ -6,20 +6,36 @@ var fs = require("fs");
 router.post('/', function (req, res) {
     try {
         res.setHeader('Content-Type', 'application/json');
+        console.log(req.body);
+
         console.log("Sensor data", req.body);
-        let eachObj = req.body;
-        if (parseInt(eachObj.light) > 300) {
-            eachObj.alert = "Beep due to light intensity greater than 300";
-        }
-        else if (parseInt(eachObj.temperature) > 10) {
-            eachObj.alert = "Beep is due to temperature difference greater than 10F";
-        }
-        else {
-            eachObj.alert = "-";
-        }
-        fs.writeFileSync('./public/json/sensorsData.json', JSON.stringify(req.body, undefined, 2));
+        // let eachObj = req.body;
+        // if (parseInt(eachObj.light) > 300) {
+        //     eachObj.alert = "Beep due to light intensity greater than 300";
+        // }
+        // else if (parseInt(eachObj.temperature) > 10) {
+        //     eachObj.alert = "Beep is due to temperature difference greater than 10F";
+        // }
+        // else {
+        //     eachObj.alert = "-";
+        // }
+        //fs.writeFileSync('./public/json/sensorsData.json', JSON.stringify(req.body, undefined, 2));
+
+        axios.post('https://smart-bike-share.appspot.com/sensor',
+            {userId:req.body.userId,
+                deviceId:req.body.deviceId,
+                temperature:req.body.temperature,
+                vibration:req.body.vibration,
+                light:req.body.light})
+            .then(function(response){
+                console.log(response.data);
+                res.send('respond with a resource:'+response.data);
+            }).catch(function(error){
+            console.log(error);
+        });
+
         console.log("Sensor done");
-        res.send();
+        // res.send();
     } catch (e) {
         console.log(e);
     }
