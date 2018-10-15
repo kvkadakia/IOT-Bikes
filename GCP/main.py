e0bad3fa0b2d3a72489f3aaac94538c1a6c27167
Harsh P Bajaj <harsh.rocks@hotmail.com>
2018-10-15 12:34:19 -0500

    Update main.py

--- GCP/main.py
+++ GCP/main.py
@@ -112,8 +112,70 @@ def updateDB():
 	out_of_range = False;
 	if float(lat)<minLat or float(lat)>maxLat:
 		out_of_range = True
+		merchantAuth = apicontractsv1.merchantAuthenticationType()
+		merchantAuth.name = '5KP3u95bQpv'
+		merchantAuth.transactionKey = '4Ktq966gC55GAX7S'
+
+		creditCard = apicontractsv1.creditCardType()
+		creditCard.cardNumber = "4111111111111111"
+		creditCard.expirationDate = "2020-12"
+
+		payment = apicontractsv1.paymentType()
+		payment.creditCard = creditCard
+
+		transactionrequest = apicontractsv1.transactionRequestType()
+		transactionrequest.transactionType = "authCaptureTransaction"
+		transactionrequest.amount = Decimal ('1.55')
+		transactionrequest.payment = payment
+
+
+		createtransactionrequest = apicontractsv1.createTransactionRequest()
+		createtransactionrequest.merchantAuthentication = merchantAuth
+		createtransactionrequest.refId = "MerchantID-0001"
+
+		createtransactionrequest.transactionRequest = transactionrequest
+		createtransactioncontroller = createTransactionController(createtransactionrequest)
+		createtransactioncontroller.execute()
+
+		response = createtransactioncontroller.getresponse()
+
+		if (response.messages.resultCode=="Ok"):
+			print "Transaction ID : %s" % response.transactionResponse.transId
+		else:
+			print "response code: %s" % response.messages.resultCode
 	if float(longi)<minLongi or float(longi)>maxLongi:
 		out_of_range = True
+		merchantAuth = apicontractsv1.merchantAuthenticationType()
+		merchantAuth.name = '5KP3u95bQpv'
+		merchantAuth.transactionKey = '4Ktq966gC55GAX7S'
+
+		creditCard = apicontractsv1.creditCardType()
+		creditCard.cardNumber = "4111111111111111"
+		creditCard.expirationDate = "2020-12"
+
+		payment = apicontractsv1.paymentType()
+		payment.creditCard = creditCard
+
+		transactionrequest = apicontractsv1.transactionRequestType()
+		transactionrequest.transactionType = "authCaptureTransaction"
+		transactionrequest.amount = Decimal ('1.55')
+		transactionrequest.payment = payment
+
+
+		createtransactionrequest = apicontractsv1.createTransactionRequest()
+		createtransactionrequest.merchantAuthentication = merchantAuth
+		createtransactionrequest.refId = "MerchantID-0001"
+
+		createtransactionrequest.transactionRequest = transactionrequest
+		createtransactioncontroller = createTransactionController(createtransactionrequest)
+		createtransactioncontroller.execute()
+
+		response = createtransactioncontroller.getresponse()
+
+		if (response.messages.resultCode=="Ok"):
+			print "Transaction ID : %s" % response.transactionResponse.transId
+		else:
+			print "response code: %s" % response.messages.resultCode
 		
 	if out_of_range:
 		return "{'away':true}"
